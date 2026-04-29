cea4426a1332f5ce598c7eee2facc9374760ef23
CheatMirWorld <wizercurator@gmail.com>
2026-04-29 19:08:37 +0300

    MyAntiCheat.java

---
+++ MyAntiCheat.java
@@ -0,0 +1,28 @@
+package me.yourname.anticheat;
+
+import com.github.retrooper.packetevents.PacketEvents;
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyAntiCheat extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        // Инициализация пакетного движка
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
+        PacketEvents.getAPI().load();
+    }
+
+    @Override
+    public void onEnable() {
+        PacketEvents.getAPI().init();
+        // Регистрация нашего слушателя проверок
+        PacketEvents.getAPI().getEventManager().registerListener(new ReachCheck());
+        getLogger().info("MyAntiCheat успешно запущен!");
+    }
+
+    @Override
+    public void onDisable() {
+        PacketEvents.getAPI().terminate();
+    }
+}
